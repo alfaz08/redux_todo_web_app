3782f2f75178bffa344327ea01de37fa2dfa8992
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-26 11:33:16 +0600

    modal is added into another file to reduce complexity

---
+++ src/components/tasks/AddTaskModal.jsx
@@ -0,0 +1,19 @@
+import Modal from "../ui/Modal";
+
+const AddTaskModal = ({ isOpen, setIsOpen }) => {
+  return (
+    <div>
+      <Modal
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+        title="programming hero"
+      >
+      <p>
+        df,jhkhdjkfhdjkashfksdhfkjhsfkjhsdkjhfsdkjhfk
+      </p>
+      </Modal>
+    </div>
+  );
+};
+
+export default AddTaskModal;
