e789f04539d651ad599fc37a1336835ec2fd2c95
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-26 19:19:32 +0600

    user personal task is added

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,8 +1,10 @@
 import {configureStore} from '@reduxjs/toolkit'
 import taskSlice from './features/tasks/taskSlice';
+import userSlice from './features/user/userSlice';
 const store = configureStore({
   reducer: {
-    taskSlice:taskSlice
+    taskSlice:taskSlice,
+    userSlice: userSlice
   }
 })
 
