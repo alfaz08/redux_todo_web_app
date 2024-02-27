d171745afa4276cf42de1b4a732e13e6634cdbca
Alfaz Hossain <alfaz.cse@gmail.com>
2024-02-27 13:00:01 +0600

    user specific task complete on button click

--- src/components/tasks/MyTasks.jsx
+++ src/components/tasks/MyTasks.jsx
@@ -4,7 +4,7 @@ import {
 } from '@heroicons/react/24/outline';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { userTasks } from '../../redux/features/tasks/taskSlice';
+import { updateStatus, userTasks } from '../../redux/features/tasks/taskSlice';
 
 const MyTasks = () => {
  
@@ -30,7 +30,10 @@ const dispatch= useDispatch()
             <button className="grid place-content-center" title="Details">
               <DocumentMagnifyingGlassIcon className="w-5 h-5 text-primary" />
             </button>
-            <button className="grid place-content-center" title="Done">
+            <button 
+            onClick={()=>dispatch(updateStatus({id:item.id,status:'done'}))}
+            className="grid place-content-center"
+             title="Done">
               <CheckIcon className="w-5 h-5 text-primary" />
             </button>
           </div>
